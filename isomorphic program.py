1ec30992862d275286496a8be97280846a553957
akshaykumar192110006 <112461426+akshaykumar192110006@users.noreply.github.com>
2022-09-26 10:56:58 +0530

    Add files via upload

---
+++ isomorphic program.py
@@ -0,0 +1,18 @@
+n1=input("enter string 1 :")
+n2=input("enter string 2:")
+def isomorphic(str1,str2):
+    if len(str1)!=len(str2):
+        return False
+    else:
+        map1,map2={},{}
+        for i in range (len(str1)):
+            ch1,ch2=str1[i],str2[i]
+            if ch1 not in map1:
+                map1[ch1]=ch2
+            if ch2 not in map2:
+                    map2[ch2]=ch1
+            if map1[ch1] !=ch2 or map2[ch2] !=ch1:
+                return False
+        return True
+print(isomorphic(n1,n2))
+            
